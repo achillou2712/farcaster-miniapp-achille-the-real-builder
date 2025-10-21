580bbd1edad85dfb51ef69358d45eebd8486b5b9
achillou2712 <achillebinkscrypto@gmail.com>
2025-10-21 19:27:04 +0200

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,26 +4,23 @@ import { APP_URL } from "../../../lib/constants";
 export async function GET() {
   const farcasterConfig = {
   // TODO: Add your own account association
-    accountAssociation: {
-    "header": "eyJmaWQiOjMyODg1NSwidHlwZSI6ImF1dGgiLCJrZXkiOiIweERCOTczZjliNWI0MDhFMkZFNjNFQ2UzNjBEYjFjODU5YjQ5NDFFRTcifQ",
-    "payload": "eyJkb21haW4iOiJmYXJjYXN0ZXItbWluaWFwcC10ZW1wbGF0ZS1lYm9uLnZlcmNlbC5hcHAifQ",
-    "signature": "1bH6yx1yjJgF2olNGcmhAgsbS229TmERlvHsktnOqA88aDMQAPm+WSFEgqfIFtcRFIUsewD5Of72EVNKufxX3Bs="
-  },
-    frame: {
-      version: "1",
-      name: "Trankil Farcaster MiniApp",
-      iconUrl: `${APP_URL}/images/icon.png`,
-      homeUrl: `${APP_URL}`,
-      imageUrl: `${APP_URL}/images/feed.png`,
-      screenshotUrls: [],
-      tags: ["Base", "farcaster", "miniapp", "template"],
-      primaryCategory: "developer-tools",
-      buttonTitle: "Launch Template",
-      splashImageUrl: `${APP_URL}/images/splash.png`,
-      splashBackgroundColor: "#ffffff",
-      webhookUrl: `${APP_URL}/api/webhook`,
-    },
-  };
+"accountAssociation": {
+"header": "eyJmaWQiOjkwMjQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDliQ0U2YmY4ZjZCQTYxMzY1YThDMmRDNTQ5YWQwOGI4MDhjNzZjNUUifQ",
+"payload": "eyJkb21haW4iOiJmYXJjYXN0ZXItbWluaWFwcC1ieS1hY2hpbGxlYnVpbGRlci52ZXJjZWwuYXBwIn0",
+"signature": "80Ntm35VVkX1f7+pKhA9BD/k9ojmlwH8QHuFuvIWVvtLPMQJmlAxEUOjnaczD5uoLBYLWsQd2f4fKqUTYgKoTRs="
+},
+"frame": {
+"version": "1",
+"name": "Example Frame",
+"iconUrl": "https://farcaster-miniapp-by-achillebuilder.vercel.app/icon.png",
+"homeUrl": "https://farcaster-miniapp-by-achillebuilder.vercel.app",
+"imageUrl": "https://farcaster-miniapp-by-achillebuilder.vercel.app/image.png",
+"buttonTitle": "Check this out",
+"splashImageUrl": "https://farcaster-miniapp-by-achillebuilder.vercel.app/splash.png",
+"splashBackgroundColor": "#eeccff",
+"webhookUrl": "https://farcaster-miniapp-by-achillebuilder.vercel.app/api/webhook"
+}
+}
 
   return NextResponse.json(farcasterConfig);
 }
