285edf85f48896e8b600fc85b4c8c4d617e96a63
achillou2712 <achillebinkscrypto@gmail.com>
2025-10-22 12:12:34 +0200

    Update WalletActions.tsx

--- components/Home/WalletActions.tsx
+++ components/Home/WalletActions.tsx
@@ -77,25 +77,6 @@ export function WalletActions() {
               No
             </button>
             </div>
-              <div className="flex flex-col space-y-2 border border-[#333] p-4 rounded-md">
-                <h2 className="text-lg font-semibold text-left">
-                  Is Base token launch before December?
-                </h2>
-               <div className="flex space-x-4">
-            <button
-              onClick={() => handleSend('Yes')}
-              className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600"
-            >
-              Yes
-            </button>
-            <button
-              onClick={() => handleSend('No')}
-              className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
-            >
-              No
-            </button>
-            </div>
-
                 
               </div>
             ) : (
